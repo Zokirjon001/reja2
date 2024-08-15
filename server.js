a53b77372da8d45c8cfa7ad4ef467148b2e0b747
Zokirjon001 <zokirjon0798@gmail.com>
2024-08-15 11:19:12 +0900

    views

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-app.get("/", function (req,res) {
-    res.end(`<h1 style="background: red">Hello World by Steve </h1>`);
+app.post("/create-item", (req,res) => {
+    console.log(req.body);
+    res.json({ test: "success"});
 });
-
-app.get("/gift", function (req,res) {
-    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-});
+app.get("./", function(req, res) {
+    res.render("harid")
+})
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
